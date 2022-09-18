bd6a305ac90346b7104c0f61a784919b7430da38
Marina <obluch@bk.ru>
2022-09-18 15:47:29 +0300

    Добавили задачу 1

---
+++ Задача_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 1");
+int number1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2=Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+{
+    Console.WriteLine($"{number1}>{number2} max={number1}");
+}
+else
+{
+    Console.WriteLine($"{number2}>{number1} max={number2}");
+}
