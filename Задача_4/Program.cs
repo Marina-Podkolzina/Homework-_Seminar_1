4f0e43536ae25f206cfe8a3e726cb0be8aa04810
Marina <obluch@bk.ru>
2022-09-18 16:30:30 +0300

    Добавили задачу 4

---
+++ Задача_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите целое число >0");
+int number1=Convert.ToInt32 (Console.ReadLine ());
+int number2=2;
+while (number2<=number1)
+{
+    Console.Write (number2+",");
+     number2=number2+2;
+}
+Console.WriteLine ();
